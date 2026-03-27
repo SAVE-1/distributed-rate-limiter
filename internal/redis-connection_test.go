4fafd6ee8ba9bed055f0f86e0f2234a6386c4b73
Default <samiv360@gmail.com>
2026-03-27 12:36:35 +0200

    test: start stored procedure tests

---
+++ internal/redis-connection_test.go
@@ -0,0 +1,13 @@
+package internal
+
+import (
+	"testing"
+)
+
+// we're going with test containers here to test the lua scripts
+// I had a small but rather annoying bug, instead of [int64 int64 int64 int64 int64]
+// I got [int64 int64 string int64 int64]
+func TestRateLimiter(t *testing.T) {
+
+}
+
